c0a7ad110797a6e423ab9d67c8bbd238b30eb9ce
nadagamaall22 <nadagamaal22@gmail.com>
2022-10-09 16:48:33 +0200

    added another file for testing pygame

---
+++ COMM_GUI/Testing pygame library/test_pygame.py
@@ -0,0 +1,41 @@
+import pygame
+import rospy
+from std_msgs.msg import String
+import sys
+pygame.init()
+
+def publisher(data):
+    rospy.init_node("keyboard input", anonymous=True)
+    pub=rospy.Publisher("/topic1",String,queue_size=10)
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        pub.publish(data)
+        rate.sleep()
+
+while True:
+    display = pygame.display.set_mode((200, 200))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+     # checking if keydown event happened or not
+    if event.type == pygame.KEYDOWN:    
+        # checking if key "A" was pressed
+        if event.key == pygame.K_a:
+            print("Key A has been pressed")
+            data="A"
+            publisher(data)
+
+        if event.key == pygame.K_b:
+            print("Key B has been pressed")
+            data="B"
+            publisher(data)
+    
+
+
+
+
+
+
+
