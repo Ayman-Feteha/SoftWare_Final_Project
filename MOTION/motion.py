a63ee8b314b6e1d56f412fa93cc8ba6c5895e73c
unknown <Osamaawadallah10@gmail.com>
2022-10-12 18:48:07 +0200

    motion not done

---
+++ MOTION/motion.py
@@ -0,0 +1,69 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+
+
+class motion():
+
+    def motorSpeed(self, axis, max_speed):
+
+
+        self.forward = axis[0]
+        self.rotate = axis[1]
+        self.slide = axis[2]
+        self.pitch = axis[3]
+        self.up = axis[4]
+        self.max_speed = max_speed
+
+        self.FR = (self.forward - self.rotate + self.slide) * self.max_speed
+        self.FL = (-self.forward - self.rotate + self.slide) * self.max_speed
+        self.TF = (self.pitch - self.up) * self.max_speed
+        self.TB = (self.pitch + self.up) * self.max_speed
+        self.BR = (-self.forward + self.rotate + self.slide) * self.max_speed
+        self.BL = (self.forward + self.rotate + self.slide) * self.max_speed
+
+        self.speeds=[self.FR,self.FL,self.TF,self.TB,self.BR,self.BL]
+
+        x=[abs(max(self.speeds)),abs(min(self.speeds))]
+        if(max(x)>self.max_speed):
+            scale = self.max_speed / max(x)
+            for i in range(len(self.speeds)):
+                self.speeds[i]=round(self.speeds[i]*scale)
+
+
+        self.output = self.encoder()
+        self.decoder()
+        return self.output
+    def encoder(self):
+        for i in range(len(self.speeds)):
+            self.speeds[i] = round(self.speeds[i] + 200)
+            self.speeds[i] = round(self.speeds[i] / 2)
+            self.speeds[i] = chr(self.speeds[i])
+
+        self.spd = ''.join(self.speeds)
+        print(self.spd)
+        return self.spd
+
+    def decoder(self):
+        self.speeds=list(self.spd)
+        for i in range(len(self.speeds)):
+            self.speeds[i] = ord(self.speeds[i])
+            self.speeds[i] = round(self.speeds[i] * 2)
+            self.speeds[i] = round(self.speeds[i] - 200)
+
+
+        print(self.speeds)
+
+
+.
+test1 = motion()
+
+while(1):
+    test1.motorSpeed([-1,0,-1,0.7,0.3], 200)
+    time.sleep(2)
+
+
+
+
