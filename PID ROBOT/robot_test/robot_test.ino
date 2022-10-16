320881bc91ecbf87b476bd3ca951d9d65593622b
Ayman-Feteha <fetehaayman@gmail.com>
2022-10-16 14:13:18 +0200

    code setup and functions of encoder

--- PID ROBOT/robot_test/robot_test.ino
+++ PID ROBOT/robot_test/robot_test.ino
@@ -1,149 +1,74 @@
+/*
+      --documentation--
+*/
+
+/*
+#include
+*/
+
+//pins decleration
+// Motor A connections
 int enA = 9;
-int in1 = 2;
-int in2 = 3;
+int in1 = 6;
+int in2 = 7;
+int enc2 = 3; //the encoder connected with wheel of motor A
 // Motor B connections
 int enB= 10;
 int in3= 4;
 int in4= 5;
-int enc1 = 6;
-int enc2 = 7;
-int diff;
-int N_tics1,N_tics2;
-int state,tics1,tics2;
-float P_value,d_value;
+int enc1 = 2; //the encoder connected with wheel of motor B
+
+// Global Variables decleration
+int N_tics1,N_tics2;  //the number of tics counted from encoder 1 and encoder 2
+int diff; //the difference between the number of tics counted from encoder1 and encoder 2
+int state,tics1,tics2;  //the state given from serial communication to control the robot motion, tics1 & tics2 declare the state of encoder (1 or 0)
+
 void setup() {
-  // Set all the motor control pins to outputs
+  
   Serial.begin(9600);
+    
+  // Set all the motor control pins to outputs
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  for (int i=4;i<8;i++){
+    pinMode(i,OUTPUT);
+    }
+  // set encoders pins to inputs
   pinMode(enc1, INPUT);
   pinMode(enc2, INPUT);
   
   // Turn off motors - Initial state
- /* digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  analogWrite(enB,100 );
-  analogWrite(enA,100);*/
+  for (int i=4;i<8;i++){
+    digitalWrite(i,LOW);
+    }
+  // attach encoders to interrupt pins so that the N_tics will be increased as soon as the encoder reads (1) not if it reads (1) during certain time in the loop
+  attachInterrupt(digitalPinToInterrupt(enc1), Inc_tics1 , RISING);
+  attachInterrupt(digitalPinToInterrupt(enc2), Inc_tics1 , RISING);
+    
 }
 
 void loop() {
- tics1=digitalRead(enc1);
- tics2=digitalRead(enc2);
-  if (Serial.available()>0){
-     state=Serial.read();
-    }
-  if (state=='a'){
-      digitalWrite(in1, HIGH);
-      digitalWrite(in2, LOW);
-      digitalWrite(in3, LOW);
-      digitalWrite(in4, HIGH);
-      analogWrite(enB,80);
-      analogWrite(enA,80);
-       if (tics1==1){
-  N_tics1+=1;
-  }
- if (tics2==1){
-  N_tics2+=1;
-  }
-   Serial.println(N_tics1);
-  
- Serial.println(N_tics2);
-if (N_tics1>=N_tics2){
-  diff = N_tics1-N_tics2;
-  P_value=diff*5;
-  analogWrite(enB,80+P_value);
-  }
-  else{
-    diff=N_tics2-N_tics1;
-    d_value=diff*1.5;
-    analogWrite(enB,80+P_value-d_value);
-    }
-  
-    }
-    else if(state=='s'){
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  analogWrite(enB,0 );
-  analogWrite(enA,0);
-      }
-     else if(state=='d'){
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  analogWrite(enB,70 );
-  analogWrite(enA,70);
-      }
-      
-
- //Serial.println(tics1);
-  
- //Serial.println(tics2);
- Serial.println("///////////////////");
 
 }
 
-// This function lets you control spinning direction of motors
-/*void directionControl() {
-  // Set motors to maximum speed
-  // For PWM maximum possible values are 0 to 255
-  analogWrite(enA, 255);
-  analogWrite(enB, 255);
+//Function of interrupt to increase the tics number of encoder of motor B
+void Inc_tics1(){
+  N_tics1+=1;
+  }
 
-  // Turn on motor A & B
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  delay(2000);
-  
-  // Now change motor directions
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  delay(2000);
-  
-  // Turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-}*/
-/*
-// This function lets you control speed of the motors
-void speedControl() {
-  // Turn on motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  
-  // Accelerate from zero to maximum speed
-  for (int i = 0; i < 256; i++) {
-    analogWrite(enA, i);
-    analogWrite(enB, i);
-    delay(20);
+//Function of interrupt to increase the tics number of encoder of motor A
+void Inc_tics2(){
+  N_tics2+=1;
   }
   
-  // Decelerate from maximum speed to zero
-  for (int i = 255; i >= 0; --i) {
-    analogWrite(enA, i);
-    analogWrite(enB, i);
-    delay(20);
+//Function to limit the tics number of encoders to avoid exceeding the storage limit
+void limit_counter(){
+  N_tics1-=1000;
+  N_tics2-=1000;
+  }
+
+//Function to reset the tics number of encoders if the pilot stops the robot 
+void reset_counter{
+  N_tics1=N_tics2=0;
   }
   
-  // Now turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-}*/
