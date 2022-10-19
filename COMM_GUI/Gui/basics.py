7e2e5c9ba9dedb574a30dec4a7335deeda6a5282
Rewan Khaled <83522163+RewanAbdulkariem@users.noreply.github.com>
2022-10-19 20:40:01 +0200

    gui

---
+++ COMM_GUI/Gui/basics.py
@@ -0,0 +1,287 @@
+from ast import Str
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QMainWindow ,QLCDNumber,QTableWidget, QTableWidgetItem
+from PyQt5.QtGui import QPainter, QColor, QPen, QFont, QBrush
+
+import sys
+from PyQt5.QtCore import Qt, QTimer
+from matplotlib.colors import LightSource
+
+#check gripper state
+GRIPPER1_STATE = "ON"
+GRIPPER2_STATE = "OFF"
+GRIPPER3_STATE = "ON"
+GRIPPER4_STATE = "OFF"
+
+#check lightening state
+LIGHT_STATE  = "ON"
+#check if ps5 connected or not 
+PS5_STATE = "ON"
+
+#thrust speeds
+thrust_1 = 200
+thrust_2 = 200
+thrust_3 = 200
+thrust_4 = 200
+thrust_5 = 150
+thrust_6 = 150
+maxspeed =200
+
+#imu reading 
+pitch = 30
+yaw = 20
+roll = 50
+
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'ROV'
+        self.left = 200
+        self.top = 200
+        self.width = 1920
+        self.height = 1080
+
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        #self.setAutoFillBackground(True)
+        self.setStyleSheet("background:rgb(20, 20, 20)")  
+
+#----------display Max speed------------------
+        
+        self.labelMAXspeed = QLabel(self)
+        self.labelMAXspeed.setText("MAX SPEED : ")
+        self.labelMAXspeed.setGeometry(300 , 250, 260, 50)
+        self.labelMAXspeed.setFont(QFont("Arial", 30))
+        self.labelMAXspeed.setStyleSheet("QLabel {color : cyan}")
+
+        self.labelMAXspeed = QLabel(self)
+        self.labelMAXspeed.setText(str(maxspeed))
+        self.labelMAXspeed.setGeometry(570 , 250, 260, 50)
+        self.labelMAXspeed.setFont(QFont("Arial", 30))
+        self.labelMAXspeed.setStyleSheet("QLabel {color : cyan}")
+# ----------display thrust speeds-------------
+
+        #THRUST1
+        self.Thrust1 = QLCDNumber(self)
+        self.Thrust1.display(thrust_1)
+        self.Thrust1.setGeometry(60 , 85, 80, 50)
+
+        self.labelThrustName1 = QLabel(self)
+        self.labelThrustName1.setText("Thrust [1]")
+        self.labelThrustName1.setGeometry(40, 195, 120, 50)
+        self.labelThrustName1.setFont(QFont("Arial", 20))
+        self.labelThrustName1.setStyleSheet("QLabel {color : cyan}")
+
+        #THRUST2
+        self.Thrust2 = QLCDNumber(self)
+        self.Thrust2.display(thrust_2)
+        self.Thrust2.setGeometry(280, 85, 80, 50)
+
+        self.labelThrustName2 = QLabel(self)
+        self.labelThrustName2.setText("Thrust [2]")
+        self.labelThrustName2.setGeometry(270, 195, 120, 50)
+        self.labelThrustName2.setFont(QFont("Arial", 20))
+        self.labelThrustName2.setStyleSheet("QLabel {color : cyan}")
+
+        #THRUST3
+        self.Thrust3 = QLCDNumber(self)
+        self.Thrust3.display(thrust_3)
+        self.Thrust3.setGeometry(500, 85, 80, 50)
+        
+        self.labelThrustName3 = QLabel(self)
+        self.labelThrustName3.setText("Thrust [3]")
+        self.labelThrustName3.setGeometry(490, 195, 120, 50)
+        self.labelThrustName3.setFont(QFont("Arial", 20))
+        self.labelThrustName3.setStyleSheet("QLabel {color : cyan}")
+
+        #THRUST4
+        self.Thrust4 = QLCDNumber(self)
+        self.Thrust4.display(thrust_4)
+        self.Thrust4.setGeometry(720, 85, 80, 50)
+        
+
+        self.labelThrustName4 = QLabel(self)
+        self.labelThrustName4.setText("Thrust [4]")
+        self.labelThrustName4.setGeometry(700, 195, 120, 50)
+        self.labelThrustName4.setFont(QFont("Arial", 20))
+        self.labelThrustName4.setStyleSheet("QLabel {color : cyan}")
+
+        #THRUST5
+        self.Thrust5 = QLCDNumber(self)
+        self.Thrust5.display(thrust_5)
+        self.Thrust5.setGeometry(940, 85, 80, 50)
+
+
+        self.labelThrustName5 = QLabel(self)
+        self.labelThrustName5.setText("Thrust [5]")
+        self.labelThrustName5.setGeometry(920, 195, 120, 50)
+        self.labelThrustName5.setFont(QFont("Arial", 20))
+        self.labelThrustName5.setStyleSheet("QLabel {color : cyan}")
+
+        #THRUST6
+        self.Thrust6 = QLCDNumber(self)
+        self.Thrust6.display(thrust_6)
+        self.Thrust6.setGeometry(1160, 85, 80, 50)
+
+
+        self.labelThrustName6 = QLabel(self)
+        self.labelThrustName6.setText("Thrust [6]")
+        self.labelThrustName6.setGeometry(1150, 195, 120, 50)
+        self.labelThrustName6.setFont(QFont("Arial", 20))
+        self.labelThrustName6.setStyleSheet("QLabel {color : cyan}")
+
+#-----------------state of controlller-------------
+
+        self.labelPS5 = QLabel(self)
+        self.labelPS5.setText("Controller state")
+        self.labelPS5.setGeometry(1370,30,190,50)
+        self.labelPS5.setFont(QFont("Arial", 20))
+        self.labelPS5.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+
+#----------------------ON label------------------------
+        self.labelPS5ON = QLabel(self)
+        self.labelPS5ON.setText(PS5_STATE)
+        self.labelPS5ON.setGeometry(1400,210,120,50)
+        self.labelPS5ON.setFont(QFont("Arial", 40))
+        self.labelPS5ON.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+
+#------------------IMU READING------------------
+
+        self.labelimu = QLabel(self)
+        self.labelimu.setText("IMU reading")
+        self.labelimu.setGeometry(30,340,190,50)
+        self.labelimu.setFont(QFont("Arial", 25))
+        self.labelimu.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+
+        self.labelpitchvalue = QLabel(self)
+        self.labelpitchvalue.setText(str(pitch))
+        self.labelpitchvalue.setGeometry(130,400,100,50)
+        self.labelpitchvalue.setFont(QFont("Arial", 20))
+        self.labelpitchvalue.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+
+        self.labelpitch = QLabel(self)
+        self.labelpitch.setText("PITCH : ")
+        self.labelpitch.setGeometry(30,400,100,50)
+        self.labelpitch.setFont(QFont("Arial", 20))
+        self.labelpitch.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+
+        self.labelyawvalue = QLabel(self)
+        self.labelyawvalue.setText(str(yaw))
+        self.labelyawvalue.setGeometry(130,450,100,50)
+        self.labelyawvalue.setFont(QFont("Arial", 20))
+        self.labelyawvalue.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+
+        self.labelyaw = QLabel(self)
+        self.labelyaw.setText("YAW : ")
+        self.labelyaw.setGeometry(30,450,100,50)
+        self.labelyaw.setFont(QFont("Arial", 20))
+        self.labelyaw.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+
+        self.labelrollvalue = QLabel(self)
+        self.labelrollvalue.setText(str(roll))
+        self.labelrollvalue.setGeometry(130,500,100,50)
+        self.labelrollvalue.setFont(QFont("Arial", 20))
+        self.labelrollvalue.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+
+        self.labelroll = QLabel(self)
+        self.labelroll.setText("ROLL : ")
+        self.labelroll.setGeometry(30,500,100,50)
+        self.labelroll.setFont(QFont("Arial", 20))
+        self.labelroll.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+
+#--------------------display lightening system-----------------------
+        self.labellight = QLabel(self)
+        self.labellight.setText("Lightening State")
+        self.labellight.setGeometry(320,340,250,50)
+        self.labellight.setFont(QFont("Arial", 25))
+        self.labellight.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+
+#----------------------state label------------------------
+        self.labellight_state = QLabel(self)
+        self.labellight_state.setText(LIGHT_STATE)
+        self.labellight_state.setGeometry(380,520,120,50)
+        self.labellight_state.setFont(QFont("Arial", 40))
+        self.labellight_state.setStyleSheet("QLabel {color : rgb(150, 150, 150)}")
+#----------------------GRIPPERS TABLE------------------------
+        self.table = QTableWidget(self)
+        self.table.setColumnCount(2)
+        self.table.setRowCount(4)
+        self.table.setMinimumWidth(200)
+        self.table.setMinimumHeight(200)
+        self.table.setGeometry(10 , 620 , 260, 260)
+        self.table.setStyleSheet("background-color: rgb(255,255,255)")
+        self.table.setItem(0,0,QTableWidgetItem("Gripper1"))
+        #self.table.setItem(0,1,QTableWidgetItem("Gripper1"))
+        self.table.setItem(1,0,QTableWidgetItem("Gripper2"))
+        #self.table.setItem(0,0,QTableWidgetItem("Gripper2"))
+        self.table.setItem(2,0,QTableWidgetItem("Gripper3"))
+        #self.table.setItem(0,0,QTableWidgetItem("Gripper3"))
+        self.table.setItem(3,0,QTableWidgetItem("Gripper4"))
+        #self.table.setItem(0,0,QTableWidgetItem("Gripper4"))
+        
+
+
+        self.show()
+
+    def paintEvent(self, event):
+
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        
+        painter.setPen(QPen(QColor(150, 150, 150), 2))
+        painter.drawRect(10 , 10 , 1300, 310)
+#-------------------------thrust--------------------------------
+        painter.setPen(QPen(QColor(45, 50, 60), 20))      
+        painter.drawArc(30, 30, 150, 150, -90 * 16, 360 * 16)
+        painter.drawArc(250, 30, 150, 150, -90 * 16, 360 * 16)
+        painter.drawArc(470, 30, 150, 150, -90 * 16, 360 * 16)
+        painter.drawArc(690, 30, 150, 150, -90 * 16, 360 * 16)
+        painter.drawArc(910, 30, 150, 150, -90 * 16, 360 * 16)
+        painter.drawArc(1130, 30, 150, 150, -90 * 16, 360 * 16)
+
+
+        painter.setPen(QPen(Qt.cyan, 20))            
+        painter.drawArc(30, 30, 150, 150, -90 * 16, -thrust_1 /maxspeed *360* 16)
+        painter.drawArc(250, 30, 150, 150, -90 * 16, -thrust_2 /maxspeed *360* 16)
+        painter.drawArc(470, 30, 150, 150, -90 * 16, -thrust_3 /maxspeed *360* 16)
+        painter.drawArc(690, 30, 150, 150, -90 * 16, -thrust_4 /maxspeed *360* 16)
+        painter.drawArc(910, 30, 150, 150, -90 * 16, -thrust_5 /maxspeed *360* 16)
+        painter.drawArc(1130, 30, 150, 150, -90 * 16, -thrust_6 /maxspeed *360* 16)
+
+#-----------------------------IMU---------------
+
+        painter.setPen(QPen(QColor(150, 150, 150), 2))
+        painter.drawRect(10 , 330 , 260, 260) 
+
+#----------------------controller state-----------------------
+        painter.setPen(QPen(QColor(150, 150, 150), 2))
+        painter.drawRect(1330 , 10, 250, 310)      
+        if (PS5_STATE =="ON"):
+            painter.setBrush(QBrush(QColor(57, 255, 20), Qt.SolidPattern))
+        else:
+            painter.setBrush(QBrush(QColor(150, 150, 150), Qt.SolidPattern))
+        painter.drawEllipse(1400, 90, 100, 100)
+
+
+#-------------------lightening system------------------------------  
+        painter.setPen(QPen(QColor(150, 150, 150), 2))
+        painter.setBrush(QBrush(Qt.transparent, Qt.SolidPattern))
+        painter.drawRect(300 , 330, 280, 260)  
+        if (LIGHT_STATE =="ON"):
+            painter.setBrush(QBrush(QColor(57, 255, 20), Qt.SolidPattern))
+        else:
+            painter.setBrush(QBrush(QColor(150, 150, 150), Qt.SolidPattern))
+        painter.drawEllipse(370, 400, 100, 100)
+
+
+
+
+app = QApplication(sys.argv)
+ex = App()
+app.exec_()
