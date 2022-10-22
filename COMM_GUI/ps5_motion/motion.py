991dedeeca7836550bb1c321dca8af5c340f224a
Ayman-Feteha <fetehaayman@gmail.com>
2022-10-22 21:18:09 +0200

    documentation and comments added

--- COMM_GUI/ps5_motion/motion.py
+++ COMM_GUI/ps5_motion/motion.py
@@ -5,6 +5,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+"""
+            *This class is created to take the reading of axis from PS5 controller and convert these reading into speed values that control the motion of the 
+            thrusters.
+            *these speed values should be ranged from -max_speed to +max_speed
+            *the max speed should be controlled from a button in the controller
+
+"""
+
 
 
 class motion():
@@ -12,29 +20,33 @@ class motion():
     def motorSpeed(self, axis, max_speed,buttons):
 
 
-        self.forward = axis[0]
-        self.rotate = axis[1]
-        self.slide = axis[2]
-        self.pitch = axis[3]
-        self.up = axis[4]
-        self.max_speed = max_speed
+        self.forward = axis[0]      #the axis of pure forward motion, +ve value means moving forward, -ve value means moving backward
+        self.rotate = axis[1]       #the axis of pure rotate motion, +values means rotating right, -ve value means rotating left
+        self.slide = axis[2]        #the axis of pure sliding motion, +ve value means sliding to right, -ve value means sliding to left
+        self.pitch = axis[3]        #the axis of pure pitch motion, +ve value means pitching down, -ve value means pitching up
+        self.up = axis[4]           #the axis of pure up motion, +ve value means moving upwards, -ve value means moving downwards
+        self.max_speed = max_speed  #the max speed used for speed values calculations to control the sensitivity of the joystick
 
-        self.FR = (self.forward - self.rotate + self.slide) * self.max_speed
-        self.FL = (-self.forward - self.rotate + self.slide) * self.max_speed
-        self.TF = (self.pitch - self.up) * self.max_speed
-        self.TB = (self.pitch + self.up) * self.max_speed
-        self.BR = (-self.forward + self.rotate + self.slide) * self.max_speed
-        self.BL = (self.forward + self.rotate + self.slide) * self.max_speed
+        """
+        the following calculated speeds are based on assumtions for propeller positioning, signs of the equation terms can be changed if the propellers positioning is changed
 
-        self.speeds=[self.FR,self.FL,self.TF,self.TB,self.BR,self.BL]
+        """
+        self.FR = (self.forward - self.rotate + self.slide) * self.max_speed        #the equation used to calculate the speed on Forward right thruster
+        self.FL = (-self.forward - self.rotate + self.slide) * self.max_speed       #the equation used to calculate the speed on Forward left thruster
+        self.TF = (self.pitch - self.up) * self.max_speed                           #the equation used to calculate the speed on the Front motor 
+        self.TB = (self.pitch + self.up) * self.max_speed                           #the equation used to calculate the speed on the Back motor
+        self.BR = (-self.forward + self.rotate + self.slide) * self.max_speed       #the equation used to calculate the speed on the Back right motor
+        self.BL = (self.forward + self.rotate + self.slide) * self.max_speed        #the equation used to calculate the speed on the Back left motor
+
+        self.speeds=[self.FR,self.FL,self.TF,self.TB,self.BR,self.BL]   
         
 
-        x=[abs(max(self.speeds)),abs(min(self.speeds))]
-        if(max(x)>self.max_speed):
+        x=[abs(max(self.speeds)),abs(min(self.speeds))]     #array of maximum and absolute minimum speeds that will be used to scale the speeds if an overlimit speed is given out of equations
+        if(max(x)>self.max_speed):                          #checking if the absolute value of the motor with max or min speed (the speeder is taken to the condition by using the previous array) is higher than max speed given by controller.
             scale = self.max_speed / max(x)
-            for i in range(len(self.speeds)):
-               self.speeds[i]=round(self.speeds[i]*scale)
-        else:
+            for i in range(len(self.speeds)):               #scaling all speeds of thrusters for safety without changing the direction of the motion
+                self.speeds[i]=round(self.speeds[i]*scale)
+        else:                                               
             for i in range(len(self.speeds)):
                 self.speeds[i]=round(self.speeds[i])
         #print(self.speeds)
